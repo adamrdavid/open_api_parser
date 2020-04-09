efec1d4fb988f27687eb4d66248407c9052573b6
Adam David <adamrdavid@gmail.com>
2020-04-09 13:32:30 -0700

    Get tests working with yaml/erb loader

--- open_api_parser.gemspec
+++ open_api_parser.gemspec
@@ -25,7 +25,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "addressable", "~> 2.3"
   spec.add_dependency "json-schema", "~> 2.8"
 
-  spec.add_development_dependency "bundler", "~> 1.13"
+  spec.add_development_dependency "bundler", "~> 2.1"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency 'pry', '~> 0.12'
--- spec/open_api_parser/reference_spec.rb
+++ spec/open_api_parser/reference_spec.rb
@@ -106,7 +106,7 @@ RSpec.describe OpenApiParser::Reference do
 
       context "given a $ref path the same as the base path" do
         it "reuses the current document" do
-          expect(YAML).to_not receive(:load)
+          expect(YAML).to_not receive(:safe_load)
           document = {"current" => true}
           ref = OpenApiParser::Reference.new('person.yaml')
           _, referrent_doc, _ = ref.resolve('person.yaml', '', document, file_cache)
@@ -184,8 +184,8 @@ RSpec.describe OpenApiParser::Reference do
         let(:ref_path) { "standard.yaml" }
 
         before do
-          expect(YAML).to_not(
-            receive(:load_file).with(base_path))
+          expect(File).to_not(
+            receive(:read).with(base_path))
         end
 
         [
@@ -213,8 +213,8 @@ RSpec.describe OpenApiParser::Reference do
         let(:ref_path) { "another_standard.yaml" }
 
         before do
-          expect(YAML).to(
-            receive(:load_file).with(cwd_relative("spec/resources/another_standard.yaml")).and_return(document))
+          expect(File).to(
+            receive(:read).with(cwd_relative("spec/resources/another_standard.yaml")).and_return(document.to_yaml))
         end
         [
           ["", "", STANDARD_DOCUMENT, ""],
@@ -284,8 +284,8 @@ RSpec.describe OpenApiParser::Reference do
         let(:ref_path) { 'another_standard.yaml' }
 
         before do
-          expect(YAML).to(
-            receive(:load_file).with(cwd_relative("spec/resources/another_standard.yaml")).and_return(document))
+          expect(File).to(
+            receive(:read).with(cwd_relative("spec/resources/another_standard.yaml")).and_return(document.to_yaml))
         end
         [
           ['', '', true],
